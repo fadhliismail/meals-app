af7aac77f0d96c16d6194499b8b8786ab677d602
Fadhli Ismail <fadhli@Sharah-Pro.local>
2020-05-20 14:06:29 +0100

    adding meals model

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+
+const CategoryGridTile = (props) => {
+    return (
+        <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
+            <View
+                style={{
+                    ...styles.container,
+                    ...{ backgroundColor: props.color },
+                }}
+            >
+                <Text style={styles.text}>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+    },
+    container: {
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: "black",
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 6, height: 2 },
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+    },
+    text: {
+        fontFamily: "open-sans-bold",
+        fontSize: 15,
+    },
+});
+
+export default CategoryGridTile;
---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,29 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
+
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+import Colors from "../constants/Colors";
+
+const MealsNavigator = createStackNavigator(
+    {
+        Categories: CategoriesScreen,
+        CategoryMeals: CategoryMealsScreen,
+        MealDetail: MealDetailScreen,
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor:
+                    Platform.OS === "android" ? Colors.primaryColor : "",
+            },
+            headerTintColor:
+                Platform.OS === "android" ? "white" : Colors.primaryColor,
+        },
+    }
+);
+
+export default createAppContainer(MealsNavigator);
